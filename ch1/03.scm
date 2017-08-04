4cf258c22ca024dca226f83117f88825d566892a
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-08-04 12:58:28 +0800

    ch1: 03: add new max32

--- ch1/03.scm
+++ ch1/03.scm
@@ -6,6 +6,25 @@
 
 (displayln (max32 1 2 3))
 
+;;; antoher solution for max32
+(define (min2 a b)
+  (if (> a b) b a))
+
+(define (fold1 f lst)
+  (if (null? (cdr lst)) (car lst)
+    (f (car lst)
+       (fold1 f (cdr lst)))))
+
+(define (min_ lst) (fold1 min2 lst))
+
+(define (sum lst) (fold1 + lst))
+
+(define (max32_ a b c)
+  (- (sum (list a b c))
+     (min_ (list a b c))))
+
+(displayln (max32_ 1 2 3))
+
 (define (square x) (* x x))
 
 (define (max32square a b c)
